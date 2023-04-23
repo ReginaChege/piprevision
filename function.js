e963bf74722da385948fc4cf7ab978cae7200462
ReginaChege <reginachegewairimu@gmail.com>
2023-04-23 20:35:29 +0300

    sixth

--- function.js
+++ function.js
@@ -8,10 +8,10 @@ function isPrime(alger) {
     }
  }
  let alger=[1,2,3,4,5,6,7,8,9,10]
- console.log(isPrime(alger))
+ isPrime(alger)
  
 
-// An area of a rectangle is calculated as follows: area = length x width. Write a function which calculates areaOfRectangle.
+//  Write a function that calculates areaOfRectangle.
 function areaOfRectangle(length, width) {
   let area=length*width
 console.log(area)
@@ -44,8 +44,56 @@ function volumeOfRectPrism(length,width,height) {
 }
 volumeOfRectPrism(20,9,12)
 
+// Write a function that takes an array of numbers and returns a new array 
+//that contains the square of each number in the original array.
+function squareNumber(numbers){
+  let empty=[]
+  for(let n=0;n<numbers.length;n++){
+    empty.push(numbers[n] * numbers[n])
+}
+return empty
+}
+numbers=[3,4,5,6,7,8,9,2,1]
 
+console.log(squareNumber(numbers))
 
+// Write a function that takes an array of strings and returns a new array that contains only the 
+// strings that have a length of three characters.
+
+function long_words(array){
+let newWord=[]
+   for (let i = 0; i < array.length; i++) {
+   if (array[i].length===3) {
+    newWord.push(array[i])
+   }
+  
+    
+   }
+   return newWord
+}  
+let array=["The" ,"begining" ,"is" ,"bad"];
+
+console.log(long_words(array));
+
+// Write a function that takes two arrays of numbers and returns a new array
+//  that contains the intersection of the two arrays (i.e., the numbers that appear in both arrays).
+function twoArray(a,b) {
+ return  
+}
+
+// Write a function that takes an array of numbers and returns the average of all the numbers in the array.
+
+function average(arraynumbers) {
+  let sum=0
+ for(let i=0;i<arraynumbers.length;i++){
+  sum+=arraynumbers[i]
+  
+ }
+ return sum/arraynumbers.length
+}
+arraynumbers=[24,67,98,90,65,43,89,76,43]
+console.log(average(arraynumbers)
+)
 
 
 
